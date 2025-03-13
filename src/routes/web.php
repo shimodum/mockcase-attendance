f2dd6034c85d630de289ccfdfaac4ad487fadf8b
akihiro shimoduma <a.shimodum@gmail.com>
2025-03-13 09:20:32 +0900

    ルーティング（仮）

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,18 +1,82 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\AttendanceController;
+use App\Http\Controllers\StampCorrectionRequestController;
+use App\Http\Controllers\Admin\LoginController as AdminLoginController;
+use App\Http\Controllers\Admin\AdminAttendanceController;
+use App\Http\Controllers\Admin\AdminStaffController;
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| 一般ユーザー向けルート
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// 会員登録
+Route::controller(RegisterController::class)->group(function () {
+    Route::get('/register', 'showForm');
+    Route::post('/register', 'store');
 });
+
+// ログイン
+Route::controller(LoginController::class)->group(function () {
+    Route::get('/login', 'showForm');
+    Route::post('/login', 'authenticate');
+});
+
+// メール認証
+Route::get('/email/verify', function () {
+    return view('auth.verify');
+});
+Route::get('/email/verify/{id}/{hash}', function () {
+    // TODO: コントローラーに移行予定
+});
+Route::post('/email/verification-notification', function () {
+    // TODO: コントローラーに移行予定
+});
+
+// 勤怠登録・一覧・詳細
+Route::prefix('/attendance')->controller(AttendanceController::class)->group(function () {
+    Route::post('/', 'store');
+    Route::get('/list', 'index');
+    Route::get('/{id}', 'show');
+});
+
+
+/*
+|--------------------------------------------------------------------------
+| 修正申請関連（一般ユーザー・管理者共通）
+|--------------------------------------------------------------------------
+*/
+
+Route::prefix('/stamp_correction_request')->controller(StampCorrectionRequestController::class)->group(function () {
+    Route::get('/list', 'index');
+    Route::get('/approve/{attendance_correction_request}', 'showApprove');
+    Route::post('/approve/{attendance_correction_request}', 'approve');
+});
+
+
+/*
+|--------------------------------------------------------------------------
+| 管理者向けルート
+|--------------------------------------------------------------------------
+*/
+
+// 管理者ログイン
+Route::controller(AdminLoginController::class)->group(function () {
+    Route::get('/admin/login', 'showForm');
+    Route::post('/admin/login', 'authenticate');
+});
+
+// 管理者 勤怠関連
+Route::prefix('/admin/attendance')->controller(AdminAttendanceController::class)->group(function () {
+    Route::get('/list', 'index');
+    Route::get('/{id}', 'show');
+    Route::get('/staff/{id}', 'staffIndex');
+});
+
+// 管理者 スタッフ一覧
+Route::get('/admin/staff/list', [AdminStaffController::class, 'index']);
