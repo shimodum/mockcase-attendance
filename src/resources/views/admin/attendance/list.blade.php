713c3af9494768049d3ce949cd92cd58f75a0dbe
akihiro shimoduma <a.shimodum@gmail.com>
2025-03-16 07:42:36 +0900

    管理者向けのナビゲーションメニューのパーシャルファイル作成まで

---
+++ src/resources/views/admin/attendance/list.blade.php
@@ -0,0 +1,9 @@
+{{-- 勤怠一覧画面（管理者） --}}
+@extends('layouts.app')
+
+@section('nav')
+    @include('components.nav.admin_nav')
+@endsection
+
+@section('content')
+<!-- 以下、勤怠一覧の内容 -->
---
+++ src/resources/views/components/nav/admin_nav.blade.php
@@ -0,0 +1,9 @@
+{{-- 管理者向けナビゲーションメニューのパーシャルファイル --}}
+<nav class="nav-menu">
+    <ul>
+        <li><a href="/admin/attendance">勤怠一覧</a></li>
+        <li><a href="/admin/staff/list">スタッフ一覧</a></li>
+        <li><a href="/stamp_correction_request/list">申請一覧</a></li>
+        <li><a href="/logout">ログアウト</a></li>
+    </ul>
+</nav>
