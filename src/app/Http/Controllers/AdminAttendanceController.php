41819f7f67935b1a76ab5fbea58561196fa9650f
akihiro shimoduma <a.shimodum@gmail.com>
2025-03-28 10:24:00 +0900

    勤怠詳細画面（管理者）への遷移OK

--- src/app/Http/Controllers/AdminAttendanceController.php
+++ src/app/Http/Controllers/AdminAttendanceController.php
@@ -13,10 +13,19 @@ class AdminAttendanceController extends Controller
     {
         $date = $request->input('date', now()->toDateString());
 
-        $attendances = Attendance::with('user', 'breakTimes') //ユーザー名や休憩時間も一覧で取得
+        $attendances = Attendance::with('user', 'breakTimes') //勤怠情報（該当ユーザー1件分）とそのユーザー名、休憩情報も取得
             ->where('date', $date)
             ->get();
 
         return view('admin.attendance.list', compact('date', 'attendances'));
+    }
+
+    // 勤怠詳細画面（管理者）の表示
+    public function show($id)
+    {
+        $attendance = Attendance::with('user', 'breakTimes') //勤怠情報（該当ユーザー1件分）とそのユーザー名、休憩情報も取得
+            ->findOrFail($id);
+
+        return view('admin.attendance.detail', compact('attendance'));
     }
 }
